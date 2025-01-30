6b27a950ab0f97d5e3edc4ffc3c277e1f426742d
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-01-30 14:26:33 -0600

    hello world from drone mode

--- scratch/drone_mode/drone_mode.py
+++ scratch/drone_mode/drone_mode.py
@@ -4,4 +4,11 @@ import time
 
 client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
 
+root = tk.Tk()
+root.geometry("500x600")
+root.title("drone_mode")
 
+label = tk.Label(root, text = "Hello World!", font = ("Arial", 18))
+label.pack(padx = 20, pady = 20)
+
+root.mainloop()
