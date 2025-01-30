1c48f9b05b90210ac88b67b207ba5b5714cf0941
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-01-30 15:13:16 -0600

    some simple designs

--- scratch/drone_mode/drone_mode.py
+++ scratch/drone_mode/drone_mode.py
@@ -11,4 +11,7 @@ root.title("drone_mode")
 label = tk.Label(root, text = "Hello World!", font = ("Arial", 18))
 label.pack(padx = 20, pady = 20)
 
+textbox = tk.Text(root, font = ("Arial", 16))
+textbox.pack()
+
 root.mainloop()
