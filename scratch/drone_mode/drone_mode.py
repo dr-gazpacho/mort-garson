2c86cf8cbc5fe732f99d9568252d8e86167740c8
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-01-30 15:37:58 -0600

    hello world in classes

--- scratch/drone_mode/drone_mode.py
+++ scratch/drone_mode/drone_mode.py
@@ -18,4 +18,12 @@ class MyGUI:
         self.check = tk.Checkbutton(self.root, text = "Show Messagebox", font = ("Arial", 16), variable = self.check_state)
         self.check.pack(padx = 10, pady = 10)
 
+        self.button = tk.Button(self.root, text = "Show Message", font = ("Arial", 18), command=self.show_message)
+        self.button.pack(padx = 10, pady = 10)
+
         self.root.mainloop()
+
+    def show_message(self):
+        print("Hello World!")
+
+MyGUI()
