d23801747ca714dbfa98fd11693b2fd616c8be84
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-03-01 14:16:04 -0600

    add some utils

---
+++ scratch/drone_mode/utils.py
@@ -0,0 +1,15 @@
+class DroneUtils():
+
+    def __init__(self):
+        self.APDS_MAX = 65535
+
+
+    # where a 0 read from APDS correlates to 20 on midi, 65535 correlates to 108
+    # greater quantitiy of light = higher frequency tone
+    def apds_light_to_midi(self, apds_reading):
+        return int(apds_reading * 88 / self.APDS_MAX) + 20
+    
+    # use in conjunction with apds_light_to_midi, would give opposite effect
+    # this will likely be useless
+    def flip_midi(self, midi_value):
+        return 128 - midi_value
---
+++ scratch/drone_mode/utils.test.py
@@ -0,0 +1,16 @@
+from utils import DroneUtils
+
+def test_drone_utils():
+    utils = DroneUtils()
+
+    assert utils.apds_light_to_midi(0) == 20, "Zero test failed"
+    assert utils.apds_light_to_midi(65535) == 108, "Max value test failed"
+    assert utils.apds_light_to_midi(32767) == 63, "Middle value test failed"
+    assert utils.apds_light_to_midi(32767.5) == 64, "Non-integer test failed"
+
+    assert utils.flip_midi(28) == 100, "Flipping failed"
+    
+    print("All tests passed!")
+
+# Run the tests
+test_drone_utils()
