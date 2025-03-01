df54f897e3baec6adea9f9eb6f4aa9cb0117ee02
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-03-01 16:44:34 -0600

    update utils, tests

--- scratch/drone_mode/utils.py
+++ scratch/drone_mode/utils.py
@@ -7,9 +7,21 @@ class DroneUtils():
     # where a 0 read from APDS correlates to 20 on midi, 65535 correlates to 108
     # greater quantitiy of light = higher frequency tone
     def apds_light_to_midi(self, apds_reading):
+        """
+        Convert APDS values to MIDI;
+        Floor: 20, Ceiling: 108;
+        APDS_MIN -> 20, APDS_MAX -> 108
+        """
         return int(apds_reading * 88 / self.APDS_MAX) + 20
     
-    # use in conjunction with apds_light_to_midi, would give opposite effect
-    # this will likely be useless
-    def flip_midi(self, midi_value):
-        return 108 - midi_value + 20
+    def mirror_midi(self, midi_value):
+        """Mirrors MIDI; 40 -> 88, 20 -> 108"""
+        return 128 - midi_value
+    
+    def apds_light_to_low_freq(self, apds_reading):
+        """
+        Convert APDS values to LFO values;
+        Floor: 1, Ceiling: 20
+        
+        """
+        return int(apds_reading * 19 / self.APDS_MAX) + 1
--- scratch/drone_mode/utils.test.py
+++ scratch/drone_mode/utils.test.py
@@ -3,15 +3,22 @@ from utils import DroneUtils
 def test_drone_utils():
     utils = DroneUtils()
 
+    # apds_light_to_midi
     assert utils.apds_light_to_midi(0) == 20, "Zero test failed"
-    assert utils.apds_light_to_midi(65535) == 108, "Max value test failed"
-    assert utils.apds_light_to_midi(32767) == 63, "Middle value test failed"
+    assert utils.apds_light_to_midi(65535) == 108, "Max test failed"
+    assert utils.apds_light_to_midi(32767) == 63, "Mid test failed"
     assert utils.apds_light_to_midi(32767.5) == 64, "Non-integer test failed"
 
-    assert utils.flip_midi(28) == 100, "Flipping failed"
-    assert utils.flip_midi(108) == 20, "Flipping failed"
-    assert utils.flip_midi(20) == 108, "Flipping failed"
-    
+    # mirror_midi
+    assert utils.mirror_midi(28) == 100, "Mirroring failed"
+    assert utils.mirror_midi(108) == 20, "Mirroring failed"
+    assert utils.mirror_midi(20) == 108, "Mirroring failed"
+
+    # 
+    assert utils.apds_light_to_low_freq(0) == 1, "Zero test failed"
+    assert utils.apds_light_to_low_freq(65535) == 20, "Max test failed"
+    assert utils.apds_light_to_low_freq(32767) == 10, "Mid test failed"
+
     print("All tests passed!")
 
 # Run the tests
